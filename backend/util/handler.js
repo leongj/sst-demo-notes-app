2065193369c08523b0bcba3af539c290f38561f7
Jason Leong <leong.j@gmail.com>
2022-06-22 11:39:53 +1000

    create and get note

---
+++ backend/util/handler.js
@@ -0,0 +1,24 @@
+// Handler function is a wrapper for the lambdas
+// The required lambda is passed in, then executed in the try/catch
+
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      console.error(e);
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    }
+  }
+}
