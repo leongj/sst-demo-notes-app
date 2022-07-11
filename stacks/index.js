26fd2dfe01ca3003b60b73b169e68f6e571b7b05
Jason Leong <leong.j@gmail.com>
2022-07-11 15:30:52 +1000

    Added AuthStack and renamed backend folder to services

--- stacks/index.js
+++ stacks/index.js
@@ -1,13 +1,14 @@
 import { StorageStack } from "./StorageStack";
 import { ApiStack } from "./ApiStack";
+import { AuthStack } from "./AuthStack";
 
 export default function main(app) {
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
-    srcPath: "backend",
+    srcPath: "services",
     bundle: {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
 }
