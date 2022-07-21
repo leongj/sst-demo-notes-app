40110a4acaca528ae6c66c51bd49e38f7d6f7e06
Jason Leong <leong.j@gmail.com>
2022-07-21 14:21:23 +1000

    Dealt with 404

---
+++ frontend/src/containers/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound text-center">
+      <h1>&#x1F937;&#x200D;&#x2640;</h1><br/>
+      <h3>Page not found</h3>
+    </div>
+  )
+}
