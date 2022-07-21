fcd8d704a7abbb3bb499a5cc66d1a7b634da1cfd
Jason Leong <leong.j@gmail.com>
2022-07-21 15:38:40 +1000

    Basic Login page

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -12,7 +12,7 @@ const config = {
     REGION: process.env.REACT_APP_REGION,
     USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
     APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
-    IDENTITY_POOL_ID: process.env.REACT_APP.IDENTITY_POOL_ID,
+    IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
   },
 };
 
